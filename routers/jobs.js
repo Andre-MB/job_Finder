02ea36fa7bf44ac77164bd72b72f305a9348e7fc
Andre-MB <andrezinhogame15@gmail.com>
2024-04-16 17:10:13 -0300

    View vacancy

--- routers/jobs.js
+++ routers/jobs.js
@@ -7,6 +7,16 @@ router.get('/test', (req, res) => {
     res.send('deu certo')
 })
 
+// detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: { id: req.params.id }
+}).then(job => {
+    res.render('view', {
+        job
+    })
+}).catch(err => console.log(err)));
+
+// form da rota de envio
 router.get('/add', (req, res) => {
     res.render('add')
 })
